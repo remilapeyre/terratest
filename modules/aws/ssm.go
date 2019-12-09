cd2db74d5b6c69ee2efc32b687d63a734dd8e994
Rémi Lapeyre <remi.lapeyre@henki.fr>
2019-12-10 00:00:09 +0100

    Add WaitForInstance and WaitForInstanceE

--- modules/aws/ssm.go
+++ modules/aws/ssm.go
@@ -1,10 +1,13 @@
 package aws
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/gruntwork-io/terratest/modules/retry"
 	"github.com/stretchr/testify/require"
 )
 
@@ -68,4 +71,49 @@ func NewSsmClientE(t *testing.T, region string) (*ssm.SSM, error) {
 	}
 
 	return ssm.New(sess), nil
+}
+
+// WaitForInstance waits until an instance get registered in the SSM inventory
+// or makes the test fail.
+func WaitForInstance(t *testing.T, region string, instanceID string, timeout time.Duration) {
+	err := WaitForInstanceE(t, region, instanceID, timeout)
+	require.NoError(t, err)
+}
+
+// WaitForInstanceE waits until the instance get registered to the SSM inventory
+// or return an error.
+func WaitForInstanceE(t *testing.T, region string, instanceID string, timeout time.Duration) error {
+	timeBetweenRetries := 2 * time.Second
+	maxRetries := int(timeout.Seconds() / timeBetweenRetries.Seconds())
+	description := fmt.Sprintf("Waiting for %s to appear in the SSM inventory", instanceID)
+
+	_, err := retry.DoWithRetryE(t, description, maxRetries, timeBetweenRetries, func() (string, error) {
+		client, err := NewSsmClientE(t, region)
+		if err != nil {
+			return "", fmt.Errorf("failed to get SSM client: %v", err)
+		}
+		key := "AWS:InstanceInformation.InstanceId"
+		t := "Equal"
+		values := []*string{&instanceID}
+		req, resp := client.GetInventoryRequest(&ssm.GetInventoryInput{
+			Filters: []*ssm.InventoryFilter{
+				{
+					Key:    &key,
+					Type:   &t,
+					Values: values,
+				},
+			},
+		})
+		if err = req.Send(); err != nil {
+			return "", nil
+		}
+
+		if len(resp.Entities) != 1 {
+			return "", fmt.Errorf("%s is not in the SSM inventory", instanceID)
+		}
+
+		return "", err
+	})
+
+	return err
 }
